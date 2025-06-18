e03937651b67a022373b27026c07d2980c860ac6
warmth1224 <warmertouch@gmail.com>
2025-06-18 23:33:13 +0300

    create: engine

---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "MASAYOSHI YAMADA",
+  profile: '/profile.png',
+  designation: "Senior Full Stack Engineer Specializing in FinTech and SaaS Platforms",
+  description: "Hi! I'm Masayoshi Yamada — a full stack engineer who builds fast backends, clean UIs, and sleeps soundly because of test coverage. I speak JavaScript fluently, automate the boring stuff, and ship code that scales (and rarely cries in prod). 9+ years in FinTech, SaaS, and keeping CI green. Let’s build something solid.",
+  email: 'yamadamasayoshi282@gmail.com',
+  phone: '+14074369174',
+  address: 'New York, NY, USA',
+  github: 'https://github.com/caraX0',
+  facebook: 'https://www.facebook.com/abusaid.riyaz/',
+  linkedIn: 'https://www.linkedin.com/in/masayoshi-yamada-898422370/',
+  twitter: 'https://twitter.com/said7388',
+  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+  leetcode: "https://leetcode.com/said3812/",
+  devUsername: "said7388",
+  resume: "https://drive.google.com/file/d/1oXykpFnleXDkB7NDLYJ2abNdlpc_dvpG/view?usp=drive_link"
+}
