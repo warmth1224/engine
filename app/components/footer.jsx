fbc572c3942f31245e4fb183177753e918f8ac56
warmth1224 <warmertouch@gmail.com>
2025-12-28 21:32:50 -0500

    update: contact info

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -12,12 +12,12 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/masayoshi-yamada-898422370/" className="text-[#16f2b3]">Masayoshi Yamada!</Link>
+            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/masayoshi-yamada-29a3453a2/" className="text-[#16f2b3]">Masayoshi Yamada</Link>
           </p>
           <div className="flex items-center gap-5">
             <Link
               target="_blank"
-              href="https://github.com/caraX0"
+              href="https://github.com/firstninja111"
               className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
             >
               <IoStar />
